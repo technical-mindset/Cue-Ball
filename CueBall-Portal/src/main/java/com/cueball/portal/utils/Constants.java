7ab38f74346e6cdee9d9b013265a8c7e512e8d97
fayez <m.fayez131@gmail.com>
2025-04-21 22:04:21 +0500

    public ip changed

--- CueBall-Portal/src/main/java/com/cueball/portal/utils/Constants.java
+++ CueBall-Portal/src/main/java/com/cueball/portal/utils/Constants.java
@@ -31,7 +31,7 @@ public interface Constants {
     /**
      * Base URL For Stage
      */
-    String RA_BASE_URL = "http://3.148.235.98:8080";
+    String RA_BASE_URL = "http://18.217.253.197:8080";
     String RA_COMMON_WEB_PORT = "/Admin-commonAdmin";
     String DONATION_PORT = "/CueBallPortal";
 
