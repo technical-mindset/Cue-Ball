582a00bc49e352465d15cf181fe932b2c564a122
asadZaidi <asadzaidi201999@gmail.com>
2025-05-21 01:56:34 +0500

    Updating the Room-Repository for Queries room availability!

--- CueBall-DAL/src/main/java/com/cueballdb/repository/RoomRepository.java
+++ CueBall-DAL/src/main/java/com/cueballdb/repository/RoomRepository.java
@@ -17,7 +17,7 @@ public interface RoomRepository extends JpaRepository<Room, Integer>, RoomCustom
 
     List<Room> findAllByEnableTrue();
 
-    @Query("SELECT r FROM Room r WHERE r.roomCategory.id = :roomCategoryId")
+    @Query("SELECT r FROM Room r WHERE r.roomCategory.id = :roomCategoryId AND r.enable = true")
     List<Room> findAllByRoomCategory(@Param("roomCategoryId") Integer roomCategoryId);
 
 
@@ -25,35 +25,34 @@ public interface RoomRepository extends JpaRepository<Room, Integer>, RoomCustom
 
     @Query(value = "SELECT COUNT(*) FROM room r " +
             "LEFT JOIN booking b ON r.id = b.room_id " +
-            "AND NOT " +
-            "( " +
-            " GREATEST(b.time_out, b.check_out) <= CURRENT_TIMESTAMP " +
-            "OR " +
-            "LEAST(b.time_in, b.check_in) >= CURRENT_TIMESTAMP " +
+            "AND NOT ( " +
+            " GREATEST(COALESCE(b.time_out, '1970-01-01'), COALESCE(b.check_out, '1970-01-01')) <= CURRENT_TIMESTAMP " +
+            " OR " +
+            " LEAST(COALESCE(b.time_in, '9999-12-31'), COALESCE(b.check_in, '9999-12-31')) >= CURRENT_TIMESTAMP " +
             ") " +
             "WHERE b.room_id IS NULL", nativeQuery = true)
     long findAvailableRoomsCount();
 
 
+
     @Query(value = "SELECT r.* FROM room r " +
             "LEFT JOIN booking b ON r.id = b.room_id " +
-            "AND NOT " +
-            "( " +
-            " GREATEST(b.time_out, b.check_out) <= CURRENT_TIMESTAMP " +
-            "OR " +
-            "LEAST(b.time_in, b.check_in) >= CURRENT_TIMESTAMP " +
+            "AND NOT ( " +
+            " GREATEST(COALESCE(b.time_out, '1970-01-01'), COALESCE(b.check_out, '1970-01-01')) <= CURRENT_TIMESTAMP " +
+            " OR " +
+            " LEAST(COALESCE(b.time_in, '9999-12-31'), COALESCE(b.check_in, '9999-12-31')) >= CURRENT_TIMESTAMP " +
             ") " +
             "WHERE b.room_id IS NULL AND r.room_category_id = :roomCategoryId", nativeQuery = true)
     List<Room> findAvailableRoomsByCategory(@Param("roomCategoryId") Integer roomCategoryId);
 
 
+
     @Query(value = "SELECT r.* FROM room r " +
             "LEFT JOIN booking b ON r.id = b.room_id " +
-            "AND NOT " +
-            "( " +
-            " GREATEST(b.time_out, b.check_out) <= CURRENT_TIMESTAMP " +
-            "OR " +
-            "LEAST(b.time_in, b.check_in) >= CURRENT_TIMESTAMP " +
+            "AND NOT ( " +
+            " GREATEST(COALESCE(b.time_out, '1970-01-01'), COALESCE(b.check_out, '1970-01-01')) <= CURRENT_TIMESTAMP " +
+            " OR " +
+            " LEAST(COALESCE(b.time_in, '9999-12-31'), COALESCE(b.check_in, '9999-12-31')) >= CURRENT_TIMESTAMP " +
             ") " +
             "WHERE b.room_id IS NOT NULL AND r.room_category_id = :roomCategoryId", nativeQuery = true)
     List<Room> findNotAvailableRoomsByCategory(@Param("roomCategoryId") Integer roomCategoryId);
