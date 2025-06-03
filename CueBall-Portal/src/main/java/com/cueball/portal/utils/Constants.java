5f29c29a60364e27f552c1588145de05878ed993
fayez <m.fayez131@gmail.com>
2025-06-04 01:42:56 +0500

    deployment

--- CueBall-Portal/src/main/java/com/cueball/portal/utils/Constants.java
+++ CueBall-Portal/src/main/java/com/cueball/portal/utils/Constants.java
@@ -28,7 +28,7 @@ public interface Constants {
     /**
      * Base URL For Stage
      */
-    String RA_BASE_URL = "http://3.147.42.225:8080";
+    String RA_BASE_URL = "http://3.129.253.250:8080";
     String PORT = "/CueBallPortal";
 
 
